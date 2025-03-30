4aa3821cd497711058a66ea024946696918f0489
Martin Lara <martinlarap127@gmail.com>
2025-03-30 17:19:18 -0600

    update add keywords

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,43 +1,43 @@
-import type { Routes } from "@angular/router";
+import type { Routes } from '@angular/router';
 
 export const routes: Routes = [
-	{
-		path: "about",
-		loadComponent: () =>
-			import("./pages/about-page/about-page.component").then(
-				(m) => m.AboutPageComponent,
-			),
-	},
-	{
-		path: "contact",
-		loadComponent: () =>
-			import("./pages/contact-page/contact-page.component").then(
-				(m) => m.ContactPageComponent,
-			),
-	},
-	{
-		path: "pricing",
-		loadComponent: () =>
-			import("./pages/pricing-page/pricing-page.component").then(
-				(m) => m.PricingPageComponent,
-			),
-	},
-	{
-		path: "pokemons",
-		loadComponent: () =>
-			import("./pages/pokemons-page/pokemons-page.component").then(
-				(m) => m.PokemonsPageComponent,
-			),
-	},
-	{
-		path: "pokemon/:id",
-		loadComponent: () =>
-			import("./pages/pokemon-page/pokemon-page.component").then(
-				(m) => m.PokemonPageComponent,
-			),
-	},
-	{
-		path: "**",
-		redirectTo: "/about",
-	},
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./pages/about-page/about-page.component').then(
+        (m) => m.AboutPageComponent
+      ),
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./pages/contact-page/contact-page.component').then(
+        (m) => m.ContactPageComponent
+      ),
+  },
+  {
+    path: 'pricing',
+    loadComponent: () =>
+      import('./pages/pricing-page/pricing-page.component').then(
+        (m) => m.PricingPageComponent
+      ),
+  },
+  {
+    path: 'pokemons',
+    loadComponent: () =>
+      import('./pages/pokemons-page/pokemons-page.component').then(
+        (m) => m.PokemonsPageComponent
+      ),
+  },
+  {
+    path: 'pokemon/:id',
+    loadComponent: () =>
+      import('./pages/pokemon-page/pokemon-page.component').then(
+        (m) => m.PokemonPageComponent
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '/pokemons',
+  },
 ];
--- src/app/pages/pokemons-page/pokemons-page.component.ts
+++ src/app/pages/pokemons-page/pokemons-page.component.ts
@@ -1,72 +1,78 @@
 import {
-	ApplicationRef,
-	ChangeDetectionStrategy,
-	Component,
-	inject,
-	OnDestroy,
-	type OnInit,
-	signal,
-} from "@angular/core";
-import { PokemonListComponent } from "../../pokemons/components/pokemon-list/pokemon-list.component";
-import { PokemonsService } from "../../pokemons/services/pokemons.service";
-import type { SimplePokemon } from "../../pokemons/interfaces/simple-pokemon.interface";
-import { ActivatedRoute, Router } from "@angular/router";
-import { toSignal } from "@angular/core/rxjs-interop";
-import { map, tap } from "rxjs";
-import { Title } from "@angular/platform-browser";
-import { PokemonListSkeletonComponent } from "./ui/pokemon-list-skeleton/pokemon-list-skeleton.component";
+  ApplicationRef,
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnDestroy,
+  type OnInit,
+  signal,
+} from '@angular/core';
+import { PokemonListComponent } from '../../pokemons/components/pokemon-list/pokemon-list.component';
+import { PokemonsService } from '../../pokemons/services/pokemons.service';
+import type { SimplePokemon } from '../../pokemons/interfaces/simple-pokemon.interface';
+import { ActivatedRoute, Router } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map, tap } from 'rxjs';
+import { Meta, Title } from '@angular/platform-browser';
+import { PokemonListSkeletonComponent } from './ui/pokemon-list-skeleton/pokemon-list-skeleton.component';
 
 @Component({
-	selector: "app-pokemons-page",
-	imports: [PokemonListComponent, PokemonListSkeletonComponent],
-	templateUrl: "./pokemons-page.component.html",
-	changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-pokemons-page',
+  imports: [PokemonListComponent, PokemonListSkeletonComponent],
+  templateUrl: './pokemons-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PokemonsPageComponent implements OnInit {
-	// private appRef = inject(ApplicationRef);
-	// Sirve para detectar cuando el componente se ha renderizado
-	// private $appState = this.appRef.isStable.subscribe((isStable) => {
-	//   console.log({ isStable });
-	// });
-	// Si se subscribe a nivel de componente, se debe desuscribir
-	// ngOnDestroy(): void {
-	//   this.$appState.unsubscribe();
-	// }
-	private pokemonsService = inject(PokemonsService);
-	public isLoading = signal(true);
-	public pokemons = signal<SimplePokemon[]>([]);
+  // private appRef = inject(ApplicationRef);
+  // Sirve para detectar cuando el componente se ha renderizado
+  // private $appState = this.appRef.isStable.subscribe((isStable) => {
+  //   console.log({ isStable });
+  // });
+  // Si se subscribe a nivel de componente, se debe desuscribir
+  // ngOnDestroy(): void {
+  //   this.$appState.unsubscribe();
+  // }
+  private pokemonsService = inject(PokemonsService);
+  public isLoading = signal(true);
+  public pokemons = signal<SimplePokemon[]>([]);
 
-	private route = inject(ActivatedRoute);
-	private router = inject(Router);
-	private title = inject(Title);
-	public currentPage = toSignal(
-		this.route.queryParamMap.pipe(
-			map((params) => params.get("page") ?? "1"),
-			map((page) => (Number.isNaN(+page) ? 1 : +page)),
-			map((page) => Math.max(1, page)),
-		),
-	);
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private title = inject(Title);
+  private meta = inject(Meta);
+  public currentPage = toSignal(
+    this.route.queryParamMap.pipe(
+      map((params) => params.get('page') ?? '1'),
+      map((page) => (Number.isNaN(+page) ? 1 : +page)),
+      map((page) => Math.max(1, page))
+    )
+  );
 
-	ngOnInit(): void {
-		// this.route.queryParamMap.subscribe(console.log);
-		this.isLoading.set(false);
-		this.loadPokemons();
-	}
+  ngOnInit(): void {
+    // this.route.queryParamMap.subscribe(console.log);
+    this.isLoading.set(false);
+    this.loadPokemons();
+    this.meta.updateTag({
+      name: 'keywords',
+      content:
+        'pokemons, pokemon, pokemon list, pokemon page, Garuda, Garuda proyects',
+    });
+  }
 
-	public loadPokemons(nextPage = 0) {
-		const pageToLoad = (this.currentPage() ?? 1) + nextPage;
-		this.pokemonsService
-			.loadPage(pageToLoad)
-			.pipe(
-				tap(() =>
-					this.router.navigate([], { queryParams: { page: pageToLoad } }),
-				),
-				tap(() =>
-					this.title.setTitle(
-						`Pokémons SSR - Page ${pageToLoad === 0 ? 1 : pageToLoad}`,
-					),
-				),
-			)
-			.subscribe(this.pokemons.set);
-	}
+  public loadPokemons(nextPage = 0) {
+    const pageToLoad = (this.currentPage() ?? 1) + nextPage;
+    this.pokemonsService
+      .loadPage(pageToLoad)
+      .pipe(
+        tap(() =>
+          this.router.navigate([], { queryParams: { page: pageToLoad } })
+        ),
+        tap(() =>
+          this.title.setTitle(
+            `Pokémons SSR - Page ${pageToLoad === 0 ? 1 : pageToLoad}`
+          )
+        )
+      )
+      .subscribe(this.pokemons.set);
+  }
 }
