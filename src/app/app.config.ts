819c0a6880173607ef278df96b9e6586cf167510
Martin Lara <martinlarap127@gmail.com>
2025-04-04 19:58:22 -0600

    Zoneless added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,21 @@
 import {
-	type ApplicationConfig,
-	provideZoneChangeDetection,
-} from "@angular/core";
-import { provideRouter } from "@angular/router";
+  type ApplicationConfig,
+  provideExperimentalZonelessChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
 
-import { routes } from "./app.routes";
+import { routes } from './app.routes';
 import {
-	provideClientHydration,
-	withEventReplay,
-} from "@angular/platform-browser";
-import { provideHttpClient, withFetch } from "@angular/common/http";
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-	providers: [
-		provideZoneChangeDetection({ eventCoalescing: true }),
-		provideRouter(routes),
-		provideClientHydration(withEventReplay()),
-		provideHttpClient(withFetch()),
-	],
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch()),
+  ],
 };
